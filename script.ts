fa23cf693a2730a0aa03469349960c37e7118827
Skordio <skordion@hotmail.com>
2024-04-10 20:58:32 -0400

    script can now find beastsaber level ids

--- script.ts
+++ script.ts
@@ -6,11 +6,10 @@ import BeastSaber from 'beastsaber-api';
 const bsapi = new BeastSaber();
 
 // Function to read the JSON data from the file
-const readDataFromFile = (filePath: string) => {
+const readDataFromFile = async (filePath: string) => {
     try {
         const data = fs.readFileSync(filePath, 'utf8');
-        const jsonData = JSON.parse(data);
-        console.log(jsonData);
+        return JSON.parse(data);
     } catch (err) {
         console.error('Error reading the file:', err);
     }
@@ -26,15 +25,72 @@ const getHomeDirectory = (username?: string) => {
     }
 };
 
-const main = () => {
+const beatSaverMapID = (mapId: string) => {
+    return mapId.replace("custom_level_", "");
+}
+
+const extractBeastSaberMapKey = (songHashDataKey: string) => {
+    let startIndex = songHashDataKey.indexOf("CustomLevels\\");
+    startIndex += "CustomLevels\\".length;
+    let endIndex = startIndex;
+    let char = songHashDataKey.charAt(endIndex);
+    while (char !== ' ') {
+        endIndex++;
+        char = songHashDataKey.charAt(endIndex);
+    }
+    return songHashDataKey.substring(startIndex, endIndex);
+
+}
+
+const beastSaberMapKey = (songHashData: any, beatSaverMapID: string) => {
+    for (const key in songHashData) {
+        if (songHashData[key].songHash === beatSaverMapID) {
+            return extractBeastSaberMapKey(key);
+        }
+    }
+    return 'Not Found';
+}
+
+const main = async () => {
     // Process command line arguments
     const usernameFlagIndex = process.argv.indexOf('--username');
     const username = usernameFlagIndex !== -1 ? process.argv[usernameFlagIndex + 1] : undefined;
+    const beatSaberDirectoryFlagIndex = process.argv.indexOf('--beatsaber-dir');
+    let beatSaberDirectory = beatSaberDirectoryFlagIndex !== -1 ? process.argv[beatSaberDirectoryFlagIndex + 1] : undefined;
+    if (!beatSaberDirectory) {
+        try {
+            beatSaberDirectory = fs.readFileSync('./secrets/beat_saber_dir', 'utf8');
+        } catch (err) { 
+            console.error('Please provide the path to the Beat Saber directory using the --beatsaber-dir flag or set beat saber path in /info/beat_saber_dir file.');
+            return;
+        }
+    }
 
     const homeDirectory = getHomeDirectory(username);
-    const filePath = path.join(homeDirectory, 'AppData\\LocalLow\\Hyperbolic Magnetism\\Beat Saber\\PlayerData.dat');
+    const playerDataFilePath = path.join(homeDirectory, 'AppData\\LocalLow\\Hyperbolic Magnetism\\Beat Saber\\PlayerData.dat');
+    const songHashDataFilePath = path.join(beatSaberDirectory, 'UserData\\SongCore\\SongHashData.dat');
 
-    readDataFromFile(filePath);
+    // console.log(songHashDataFilePath)
+
+    const playerData = await readDataFromFile(playerDataFilePath);
+    const songHashData = await readDataFromFile(songHashDataFilePath);
+
+    // console.log(playerData.localPlayers[0].favoritesLevelIds)
+    
+    for (const levelId of playerData.localPlayers[0].favoritesLevelIds) {
+        let bSaberMapKey = beastSaberMapKey(songHashData, beatSaverMapID(levelId));
+        if (bSaberMapKey !== 'Not Found') {
+            console.log(bSaberMapKey);
+            break;
+        }
+    }
+    // let mapId = beatSaverMapID(playerData.localPlayers[0].favoritesLevelIds[0])
+    // let bSaberMapKey = beastSaberMapKey(songHashData, mapId);
+    
+    // console.log(bSaberMapKey);
+    // let map = await bsapi.getMapByKey('1dd');
+    
+    // console.log(map);
 };
 
 main();
